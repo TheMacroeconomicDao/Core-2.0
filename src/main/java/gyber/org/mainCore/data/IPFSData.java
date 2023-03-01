eee73a887dc5c508e1bce7af6591c08b01a1da0c
Nicki <casperplay45@gmail.com>
2023-03-01 19:23:55 +0200

    fix Exception 'persistence xml'

--- src/main/java/gyber/org/mainCore/data/IPFSData.java
+++ src/main/java/gyber/org/mainCore/data/IPFSData.java
@@ -1,11 +1,14 @@
 package gyber.org.mainCore.data;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "users_ipfs_data")
 public class IPFSData {
 
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
 }
