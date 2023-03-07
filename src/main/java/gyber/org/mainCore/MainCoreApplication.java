23603bb4905d06265a2fbe7e3dc857ec4f1278b8
Nicki <casperplay45@gmail.com>
2023-03-07 20:36:52 +0200

    start fix transactions

--- src/main/java/gyber/org/mainCore/MainCoreApplication.java
+++ src/main/java/gyber/org/mainCore/MainCoreApplication.java
@@ -2,9 +2,12 @@ package gyber.org.mainCore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "gyber.org.mainCore.data.repository")
+@EntityScan(basePackages = "gyber.org.mainCore.data.entities")
 public class MainCoreApplication {
 
 	public static void main(String[] args) {
