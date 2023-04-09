fbf89e25a8b29e49e2366187737b77830d43044f
Aomisha <vlasenko12ava@gmail.com>
2023-04-09 13:25:47 +0500

    Написал security с access и refresh токенами

--- src/main/java/gyber/org/mainCore/MainCoreApplication.java
+++ src/main/java/gyber/org/mainCore/MainCoreApplication.java
@@ -22,23 +22,7 @@ public class MainCoreApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MainCoreApplication.class, args);
 	}
-	@Bean
-	CommandLineRunner run(UserService userService){
-		return args -> {
-			userService.saveRole(new Role(null, "ROLE_USER"));
-			userService.saveRole(new Role(null, "ROLE_MODERATOR"));
-			userService.saveRole(new Role(null, "ROLE_ADMIN"));
 
-			userService.saveUser(new User("vlasenko12ava@gmail.com", "Aleksandr", "Aomisha", "Vlasenko", "123", new ArrayList<>()));
-			userService.saveUser(new User("grebnev@gmail.com", "Danila", "muhamad", "Grebnev", "123", new ArrayList<>()));
-			userService.saveUser(new User("carring@mail.ru", "Aleksandr", "carring", "Smirnov", "123", new ArrayList<>()));
-
-			userService.addRoleToUser("Aomisha", "ROLE_ADMIN");
-			userService.addRoleToUser("vlasenko12ava@gmail.com", "ROLE_MODERATOR");
-			userService.addRoleToUser("grebnev@gmail.com", "ROLE_MODERATOR");
-			userService.addRoleToUser("carring", "ROLE_USER");
-		};
-	}
 
 	@Bean
 	PasswordEncoder passwordEncoder() {
